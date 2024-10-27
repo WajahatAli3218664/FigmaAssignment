4c9fa78227726df7ec51389bfe2442ac3ac5929a
UsmanFaisal13 <130107471+UsmanFaisal13@users.noreply.github.com>
2024-10-27 05:22:45 +0500

    recent post section 40% complete

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,9 +1,10 @@
+'use client'
 import React from 'react'
 
 export const Navbar = () => {
     return (
         <header className="flex justify-center lg:justify-end">
-            <nav className="flex text-black font-medium">
+            <nav className="flex text-black font-medium cursor-pointer">
                 <ul className="flex gap-12 p-12 text-xl">
                     <li>Works</li>
                     <li>Blog</li>
--- app/components/Recent.tsx
+++ app/components/Recent.tsx
@@ -1,20 +1,32 @@
+'use client'
 import React from 'react'
 
 const Recent = () => {
     return (
-        <section className='bg-[#F4E2E2] flex-col item'>
-            <div>
-                <h1>
+        <section className='bg-[#F4E2E2] flex flex-col gap-6 lg:px-40 h-[502px] w-full pt-6'>
+            <div className='flex lg:justify-between'>
+                <h1 className='font-heebo text-[22px] font-medium leading-[32.31px]'>
                     Recent posts
                 </h1>
-                <h1>
+                <h1 className='text-[#F98585] font-heebo text-[22px] font-medium leading-[32.31px]'>
                     View all
                 </h1>
-
             </div>
-            <div>
 
+            <div className='flex lg:flex-row lg:justify-between flex-col'>
+                <div className='bg-[#FFFFFF] w-[483px] h-[356px]'>
+                    <h1 className='font-heebo text-[26px] font-bold leading-[38px] tracking-normal flex justify-center'>
+                        Making a design system from <br /> scratch
+                    </h1>
+
+                </div>
+                <div className='bg-[#FFFFFF] w-[483px] h-[356px]'>
+
+
+                </div>
             </div>
+
+
 
         </section>
     )
