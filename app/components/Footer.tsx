5dc0510acd6aee5d10cd3bc52d3cc2dd41e11d8d
UsmanFaisal13 <130107471+UsmanFaisal13@users.noreply.github.com>
2024-10-27 20:31:20 +0500

    project almost complete

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,11 +1,21 @@
 import React from 'react'
+import Image from 'next/image'
 
 function Footer() {
     return (
 
-        <footer className="footer">
-            <div className="container">
-                <p>&copy; {new Date().getFullYear()} Your Website Name</p>
+        <footer className="flex items-center justify-center h-[243px] flex-col gap-10">
+            <div className='flex gap-14'>
+
+                <Image src="/fb.png" width={30} height={30} alt='logo'></Image>
+                <Image src="/insta.png" width={30} height={30} alt='logo'></Image>
+                <Image src="/Group.png" width={30} height={30} alt='logo'></Image>
+                <Image src="/Linkedin.png" width={30} height={30} alt='logo'></Image>
+
+
+            </div>
+            <div className="flex font-medium font-heebo text-[22px] leading-[32.31px]">
+                <p>Copyright ©2020 All rights reserved</p>
             </div>
         </footer>
     )
--- app/page.tsx
+++ app/page.tsx
@@ -2,18 +2,20 @@ import Hero from './components/Hero';
 import Recent from './components/Recent';
 import { Navbar } from './components/Navbar';
 import Footer from './components/Footer';
+import Featured from './components/Featured';
 
 
 
 export default function Home() {
   return (
-    <div className="h-[2453px] w-full text-black flex flex-col gap-32">
+    <div className="h-screen w-full text-black flex flex-col gap-32">
 
       <main className="flex flex-col gap-24">
 
         <Navbar />
         <Hero />
         <Recent />
+        <Featured />
         <Footer />
 
 
